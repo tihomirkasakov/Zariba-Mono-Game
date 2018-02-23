20746d87c5b5311737949baa9407f9295080ef2b
tkasakov <tihomirkasakov@gmail.com>
2018-02-24 01:21:58 +0200

    Added base Model structure with some Classes, Enums and Interfaces

---
+++ NotSoSuperMario/Model/Interfaces/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace NotSoSuperMario.Model.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface IPlayer : IUnit
+    {
+        void Jump();
+        void TakeDamage();
+    }
+}
