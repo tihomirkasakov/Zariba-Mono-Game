08a51741b075dd23a05a80ac605285902883aac1
meglenas <meglena.hs@gmail.com>
2018-03-02 00:04:42 +0200

    fixed typos

--- NotSoSuperMario/Model/Player/Player.cs
+++ NotSoSuperMario/Model/Player/Player.cs
@@ -18,12 +18,12 @@
     {
         private const float FRICTION_FORCE = 0.8f;
         private const float MAX_PLAYER_SPEED = 3;
-        private const float PLAYER_ACCEERATION = 0.1f;
+        private const float PLAYER_ACCELERATION = 0.1f;
         private const int JUMP_VELOCITY = 12;
         private const int TEMP_DISTANCE = 3;
 
         private const int LEFT_BOUND = 0;
-        private const int RIFGHT_BOUND = 1280;
+        private const int RIGHT_BOUND = 1280;
         private const int DEFAULT_SHURIKEN = 3;
         private const int MAX_HEALTH = 100;
 
@@ -66,7 +66,7 @@
             this.State = PlayerStates.IDLE;
 
             // Checking for collision with blocks underneath the player
-            this.HandleTopCollisiton(blocks);
+            this.HandleTopCollision(blocks);
             this.Position += this.velocity;
 
             // Movement
@@ -74,31 +74,7 @@
             this.HandleBottomCollision(blocks);
 
             // Side Collision
-            this.HandleSideCollisiton(blocks);
-
-        }
-
-        private void HandleTopCollisiton(List<Block> blocks)
-        {
-            if (this.velocity.Y < 0)
-            {
-                foreach (var block in blocks)
-                {
-                    Rectangle tempRect = new Rectangle((int)this.Bounds.X, (int)(this.Bounds.Y + this.velocity.Y), this.Bounds.Width, 20);
-                    if (tempRect.Intersects(block.Bounds))
-                    {
-                        this.velocity = new Vector2(this.velocity.X, 5);
-                    }
-                }
-            }
-        }
-
-        private bool IsOnTopOf(Rectangle rect)
-        {
-            return (this.Bounds.Bottom + this.velocity.Y >= rect.Top - 10) &&
-                this.Bounds.Bottom + this.velocity.Y <= rect.Top &&
-                this.Bounds.Right >= rect.Left + 4 &&
-                this.Bounds.Left <= rect.Right - 4;
+            this.HandleSideCollision(blocks);
         }
 
         private void HandleBottomCollision(List<Block> blocks)
@@ -113,18 +89,25 @@
                     this.velocity = new Vector2(this.velocity.X, 0);
                 }
             }
-
         }
 
-        private void HandleSideCollisiton(List<Block> blocks)
+        private bool IsOnTopOf(Rectangle rect)
+        {
+            return (this.Bounds.Bottom + this.velocity.Y >= rect.Top - 10) &&
+                this.Bounds.Bottom + this.velocity.Y <= rect.Top &&
+                this.Bounds.Right >= rect.Left + 4 &&
+                this.Bounds.Left <= rect.Right - 4;
+        }
+
+        private void HandleSideCollision(List<Block> blocks)
         {
             if ((this.Bounds.Left + (this.Bounds.Width / 2)) + this.velocity.X < LEFT_BOUND)
             {
-                this.Position = new Vector2(-(this.Bounds.Width/2), this.Position.Y);
+                this.Position = new Vector2(- (this.Bounds.Width / 2), this.Position.Y);
             }
-            else if ((this.Bounds.Right - (this.Bounds.Width / 2)) + this.velocity.X > RIFGHT_BOUND)
+            else if ((this.Bounds.Right - (this.Bounds.Width / 2)) + this.velocity.X > RIGHT_BOUND)
             {
-                this.Position = new Vector2(RIFGHT_BOUND- (this.Bounds.Width / 2), this.Position.Y);
+                this.Position = new Vector2(RIGHT_BOUND - (this.Bounds.Width / 2), this.Position.Y);
             }
             else
             {
@@ -138,7 +121,7 @@
                     tempDistance = -TEMP_DISTANCE;
                 }
 
-                Rectangle tempRect = new Rectangle((int)(this.Bounds.X + (this.velocity.Y + tempDistance)),
+                Rectangle tempRect = new Rectangle((int)(this.Bounds.X + (this.velocity.X + tempDistance)),
                     (int)(this.Bounds.Y), this.Bounds.Width, this.Bounds.Height);
 
                 foreach (var block in blocks)
@@ -146,6 +129,21 @@
                     if (tempRect.Intersects(block.Bounds))
                     {
                         this.velocity = new Vector2(0, this.velocity.Y);
+                    }
+                }
+            }
+        }
+
+        private void HandleTopCollision(List<Block> blocks)
+        {
+            if (this.velocity.Y < 0)
+            {
+                foreach (var block in blocks)
+                {
+                    Rectangle tempRect = new Rectangle((int)this.Bounds.X, (int)(this.Bounds.Y + this.velocity.Y), this.Bounds.Width, 20);
+                    if (tempRect.Intersects(block.Bounds))
+                    {
+                        this.velocity = new Vector2(this.velocity.X, 5);
                     }
                 }
             }
@@ -178,7 +176,7 @@
                 }
                 else if (key.Button == this.controls["Shoot"] && key.ButtonState == Controller.Utils.KeyState.Clicked)
                 {
-                     this.Attack();
+                    this.Attack();
                 }
             }
 
@@ -216,15 +214,6 @@
 
         }
 
-        private void Jump()
-        {
-            if (this.isGrounded)
-            {
-                this.isGrounded = false;
-                this.velocity = new Vector2(this.velocity.X, -JUMP_VELOCITY);
-            }
-        }
-
         private void Attack()
         {
             if (this.Shurikens > 0)
@@ -234,33 +223,40 @@
             }
         }
 
-
-
-            private void MoveLeft()
+        private void Jump()
+        {
+            if (this.isGrounded)
             {
-                if (!Keyboard.GetState().IsKeyDown(this.controls["Move Right"]))
-                {
-                    this.IsFacingRight = false;
-                }
+                this.isGrounded = false;
+                this.velocity = new Vector2(this.velocity.X, -JUMP_VELOCITY);
+            }
+        }
 
-                if (this.velocity.X > -MAX_PLAYER_SPEED)
-                {
-                    this.velocity = new Vector2(this.velocity.X - PLAYER_ACCEERATION, this.velocity.Y);
-                }
+        private void MoveLeft()
+        {
+            if (!Keyboard.GetState().IsKeyDown(this.controls["Move Right"]))
+            {
+                this.IsFacingRight = false;
             }
 
-            private void MoveRight()
+            if (this.velocity.X > -MAX_PLAYER_SPEED)
             {
-                if (!Keyboard.GetState().IsKeyDown(this.controls["Move Left"]))
-                {
+                this.velocity = new Vector2(this.velocity.X - PLAYER_ACCELERATION, this.velocity.Y);
+            }
+        }
 
-                    this.IsFacingRight = true;
-                }
+        private void MoveRight()
+        {
+            if (!Keyboard.GetState().IsKeyDown(this.controls["Move Left"]))
+            {
 
-                if (this.velocity.X < MAX_PLAYER_SPEED)
-                {
-                    this.velocity = new Vector2(this.velocity.X + PLAYER_ACCEERATION, this.velocity.Y);
-                }
+                this.IsFacingRight = true;
+            }
+
+            if (this.velocity.X < MAX_PLAYER_SPEED)
+            {
+                this.velocity = new Vector2(this.velocity.X + PLAYER_ACCELERATION, this.velocity.Y);
             }
         }
     }
+}
